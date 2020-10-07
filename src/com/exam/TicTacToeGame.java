b5b191797b13a06af4bcce1e4c25c22debc1f33e
beni2412 <hardaman97@gmail.com>
2020-10-07 09:36:01 +0530

    [Hardaman] Added: Method to determine player and computer character

--- src/com/exam/TicTacToeGame.java
+++ src/com/exam/TicTacToeGame.java
@@ -1,9 +1,18 @@
 package com.exam;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");
 		char[] board = creatingBoard();
+		char player = chooseLetter();
+		char computer = ' ';
+		if (player == 'X')
+			computer = 'O';
+		else
+			computer = 'X';
 	}
 
 	public static char[] creatingBoard() {
@@ -11,7 +20,15 @@ public class TicTacToeGame {
 		for (int i = 0; i < board.length; i++) {
 			board[i] = ' ';
 		}
-		return board;	
-		
-		}
+		return board;
+
 	}
+
+	public static char chooseLetter() {
+		System.out.println("Choose a letter X or O: ");
+		char user = sc.next().charAt(0);
+		return user;
+
+	}
+
+}
